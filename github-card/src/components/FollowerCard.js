bfdbb448c31f4da4d8a7c860c2416ddb47c0555d
Anh Truong <aqtruong97@gmail.com>
2020-06-02 14:26:30 -0400

    added reactstrap and some basic styling

--- github-card/src/components/FollowerCard.js
+++ github-card/src/components/FollowerCard.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Col
+} from 'reactstrap';
+import './FollowerCard.css'
 
 const FollowerCard = (props) => {
 
     return (
-        <div>
+        <div className="follower-container">
             {props.followers.map((follower) => (
-                <div>
-                    <h3>{follower.login}</h3>
-                </div>
+                <Col sm="4">
+                    <Card>
+                    <CardImg top width="100%" src={`${follower.avatar_url}`} />
+                    <CardBody>
+                        <CardTitle>{follower.login}</CardTitle>
+                    </CardBody>
+                    </Card>
+                </Col>
             ))}
         </div>
     )
--- github-card/src/components/UserCard.js
+++ github-card/src/components/UserCard.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Col
+} from 'reactstrap';
+import './UserCard.css';
 
 const UserCard = (props) => {
     return (
-        <div>
-            <h1>{props.user.name}</h1>
-            <h2>{props.user.login}</h2>
-            <h3>{props.user.bio}</h3>
-            <h3>Followers: {props.user.followers}</h3>
-            <h3>Following: {props.user.following}</h3>
+        <div className="user-container">
+            <Col lg="8">
+            <Card>
+                <CardImg top width="100%" src={`${props.user.avatar_url}`} />
+                    <CardBody>
+                        <CardTitle>{props.user.name}</CardTitle>
+                        <CardSubtitle>{props.user.login}</CardSubtitle>
+                        <CardText>{props.user.bio}</CardText>
+                        <CardText>Followers: {props.user.followers}</CardText>
+                        <CardText>Following: {props.user.following}</CardText>
+                    </CardBody>
+            </Card>
+            </Col>
         </div>
     )
 }
